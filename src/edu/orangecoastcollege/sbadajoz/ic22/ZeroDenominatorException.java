1eb231b915a4c8c44d05107fed8a41518088eed9
Badajoz <sbadajoz@student.cccd.edu>
2016-11-18 12:35:40 -0800

    Completed ZeroDenomiatorException

--- src/edu/orangecoastcollege/sbadajoz/ic22/ZeroDenominatorException.java
+++ src/edu/orangecoastcollege/sbadajoz/ic22/ZeroDenominatorException.java
@@ -1,6 +1,13 @@
 package edu.orangecoastcollege.sbadajoz.ic22;
+/*
+Badajoz, Seve
+CS A170
+November 18, 2016
 
-public class ZeroDenominatorException extends Exception
-{
-
+IC22
+*/
+public class ZeroDenominatorException extends Exception {
+    public ZeroDenominatorException(String message) {
+        super(message);
+    }
 }
