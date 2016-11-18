8811917ea61e7e82384387553304a7c71f5e371a
Badajoz <sbadajoz@student.cccd.edu>
2016-11-18 12:37:49 -0800

    Completed Rational class

--- src/edu/orangecoastcollege/sbadajoz/ic22/Rational.java
+++ src/edu/orangecoastcollege/sbadajoz/ic22/Rational.java
@@ -7,5 +7,39 @@ November 18, 2016
 IC22
 */
 public class Rational {
+ private int mNumerator;
+ private int mDenominator;
+ 
+ public Rational(int numerator, int denominator) throws ZeroDenominatorException{
+     mNumerator = numerator;
+     if(denominator == 0) throw new ZeroDenominatorException("Denominator cannot be zero.");
+     mDenominator = denominator;
+ }
 
+public int getnumerator()
+{
+    return mNumerator;
+}
+
+public int getDenomiator()
+{
+    return mDenominator;
+}
+
+public void setNumerator(int numerator)
+{
+    this.mNumerator = numerator;
+}
+
+public void setDenominator(int denominator) throws ZeroDenominatorException
+{
+    if(denominator == 0) throw new ZeroDenominatorException("Denominator cannot be zero.");
+    this.mDenominator = denominator;
+}
+ public String toString() {
+     if((mNumerator > 0 && mDenominator > 0) || (mNumerator < 0 && mDenominator < 0))
+     return Math.abs(mNumerator) + "/" + Math.abs(mDenominator);
+     else return "-" + Math.abs(mNumerator) + "/" + Math.abs(mDenominator);
+ }
+ 
 }
