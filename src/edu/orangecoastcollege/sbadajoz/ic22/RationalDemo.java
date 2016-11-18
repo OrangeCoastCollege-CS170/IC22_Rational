4d439bc9f8860d23053547f3fd4ac5430a41148a
Badajoz <sbadajoz@student.cccd.edu>
2016-11-18 12:43:40 -0800

    Completed RationalDemo

--- src/edu/orangecoastcollege/sbadajoz/ic22/RationalDemo.java
+++ src/edu/orangecoastcollege/sbadajoz/ic22/RationalDemo.java
@@ -7,5 +7,18 @@ November 18, 2016
 IC22
 */
 public class RationalDemo {
-
+    public static void main(String[] args) {
+        try {
+            Rational r1 = new Rational(2, 3);
+            System.out.println(r1);
+            Rational r2 = new Rational(-2, 3);
+            System.out.println(r2);
+            Rational r3 = new Rational(-2, -3);
+            System.out.println(r3);
+            Rational r4 = new Rational(2, 0);
+            System.out.println(r4);
+        } catch (ZeroDenominatorException e){
+            System.out.println(e.getMessage());
+        }
+    }
 }
